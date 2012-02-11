cf0f7871997797e226b41c291cc6cc654638d22a
Brad Ransbury <bsransbury@gmail.com>
2012-02-11 03:39:58 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pidoma::Application.config.secret_token = 'a6a18f1c7805b2de1b19a6768c7bc131a97e6d974ae61e3b6a3c1a69bc0eb5282d6affe4fc0405f3a9eeaadc2993d497abaa3d1d0751b793e6a4d36fb96bd981'
